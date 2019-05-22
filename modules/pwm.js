0ba9115cbdfda517fed0a51869ed1cfc11c51af4
Jonas Barsten <jonasbarsten@gmail.com>
2019-05-22 15:18:09 +0200

    Defining more pwm-stuff on init

--- modules/pwm.js
+++ modules/pwm.js
@@ -1,4 +1,4 @@
-//'use strict';
+'use strict';
 
 var i2cBus = require("i2c-bus");
 var Pca9685Driver = require("pca9685").Pca9685Driver;
@@ -10,27 +10,28 @@ var options = {
 	debug: true
 };
 
-	pwm = new Pca9685Driver(options, function(err) {
-  	if (err) {
+var pwm = new Pca9685Driver(options, function(err) {
+  if (err) {
 		console.error("Error initializing PCA9685");
 		process.exit(-1);
   }
+
   console.log("Initialization done");
 
-    pwm.setPulseRange(0, 42, 255);
+  pwm.setPulseRange(0, 42, 255);
 
-    // Set the pulse length to 1500 microseconds for channel 2
-    pwm.setPulseLength(0, 1500);
+  // Set the pulse length to 1500 microseconds for channel 2
+  pwm.setPulseLength(0, 1500);
 
-    function turnOn() {
-		pwm.channelOn(0);
-	}
-	function turnOff() {
-		pwm.channelOff(0);
-	}
-	turnOn();
-	setTimeout(turnOff,2000);
- });
+ //  function turnOn() {
+	// 	pwm.channelOn(0);
+	// }
+	// function turnOff() {
+	// 	pwm.channelOff(0);
+	// }
+	// turnOn();
+	// setTimeout(turnOff,2000);
+});
 
 
 exports.rotate = (channel, value) => {
